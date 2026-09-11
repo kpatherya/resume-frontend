20c7d55377749591c060a6bfd9c2c1e68ef789ea
kpath1999 <kausarpatheryaa@gmail.com>
2026-09-10 23:54:18 -0400

    visit counter added

--- public/visitcounter.js
+++ public/visitcounter.js
@@ -5,16 +5,25 @@ const options = {
     'Content-Type': 'application/json',
   }
 };
-  
-fetch('https://56uump58ii.execute-api.us-east-1.amazonaws.com/dev/', options)
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
+
+const visitTarget = document.querySelector('span.visits');
+
+if (visitTarget) {
+  fetch('https://56uump58ii.execute-api.us-east-1.amazonaws.com/dev/', options)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
       throw new Error('Network Response Error');
-    }
-  })
-  .then(data => {
-    document.querySelector('span.visits').insertAdjacentHTML('beforeend', data);
-  })
-  .catch(error => console.log('Error: ', error));
+    })
+    .then((data) => {
+      const count = typeof data === 'object'
+        ? (data.count ?? data.visits ?? Object.values(data)[0] ?? 0)
+        : data;
+      visitTarget.textContent = count;
+    })
+    .catch((error) => {
+      console.log('Error: ', error);
+      visitTarget.textContent = '0';
+    });
+}
